955f046abd7b6a4725b5b7926a3713169fced8e0
Dmytro <drepetskyi@gmail.com>
2022-12-04 21:43:20 +0200

    dfr

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 const app = express()
 
 app.get('/', (reg, res) => {
     res.send("dsfd")
 })
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${PORT}`)
+  })
